5cb07122b6071a35232d838d268d91154c14c3cb
Lewis Sewell <lewislsewell@gmail.com>
2023-07-12 18:15:13 +0100

    Add equals and clear functionality

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
 function Calculator() {
 	this.num1 = null;
-  this.op = '';
-  this.num2 = null;
+  	this.op = null;
+  	this.num2 = null;
+	this.opCount = 0;
   
 	this.add = function () {
-    return this.num1 + this.num2;
+		const num3 = this.num1;
+		if (this.num2 !== this.num2) {
+			return this.num1 + num3;
+		} 
+		return this.num1 + this.num2;
   };
   
 	this.subtract = function () {
-    return this.num1 - this.num2;
+		const num3 = this.num1;
+		if (this.num2 !== this.num2) {
+			return this.num1 - num3;
+		} 
+    	return this.num1 - this.num2;
   };
   
 	this.multiply = function () {
-    return this.num1 * this.num2;
+		const num3 = this.num1;
+		if (this.num2 !== this.num2) {
+			return this.num1 * num3;
+		} 
+    	return this.num1 * this.num2;
   };
   
 	this.divide = function () {
-    return this.num1 / this.num2;
+		const num3 = this.num1;
+		if (this.num2 === 0) {
+			return 'WTF!';
+		}
+		if (this.num2 !== this.num2) {
+			return this.num1 / num3;
+		} 
+		return this.num1 / this.num2;
   };
   
 	this.operate = function () {
     switch (this.op) {
       case '+':
-        console.log(this.add());
+        return this.add();
       case '-':
         return this.subtract();
-      case '*':
+      case 'x':
         return this.multiply();
       case '/':
         return this.divide();
@@ -33,23 +53,20 @@ function Calculator() {
   };
 	
 	this.getCurrentDisplay = function (value) {
-		const numbers = value.split(this.op);
-		this.num1 = +numbers[0];
-		this.num2 = +numbers[1];
-		console.log(this.num1);
-		console.log(this.num2);
+		this.numbers = value.split(this.op);
+		this.num1 = +this.numbers[0];
+		this.num2 = +this.numbers[1];
 	};
 	
 	this.getCurrentOperator = function (value) {
 		this.op = value;
-		console.log(this.op);
 	};
   
 	this.displayNumbers = function () {
-    	const btns = document.querySelectorAll('.numbers');
-    	btns.forEach((btn) => {
-      		btn.addEventListener('click', () => {
-        		let currentDisplayValue = document.getElementById("display").textContent += btn.textContent;
+    	const numberButtons = document.querySelectorAll('.numbers');
+    	numberButtons.forEach((numberButton) => {
+      		numberButton.addEventListener('click', () => {
+        		let currentDisplayValue = document.getElementById('display').textContent += numberButton.textContent;
 				cal.getCurrentDisplay(currentDisplayValue);
       		});
     	});
@@ -59,17 +76,42 @@ function Calculator() {
 		const operatorButtons = document.querySelectorAll('#operator-buttons');
 		operatorButtons.forEach((operatorButton) => {
 			operatorButton.addEventListener('click', () => {
-				
-				let currentDisplayValue = document.getElementById("display").textContent += operatorButton.textContent;
+				document.getElementById('display').textContent += operatorButton.textContent;
 				cal.getCurrentOperator(operatorButton.textContent);
+				cal.opCount += 1;
+				if (cal.opCount == 2) {
+					document.getElementById('display').textContent = cal.operate().toString() + operatorButton.textContent;
+					cal.opCount = 1;
+				}
 			});
 		});
 	};
+	
 	this.equals = function () {
 		const equalsButton = document.querySelector('#equals-button');
-		console.log(equalsButton);
 		equalsButton.addEventListener('click', () => {
-			cal.operate();
+			this.opCount = 0;
+			const result = cal.operate()
+			if (result === undefined) {
+				document.getElementById('display').textContent;
+			} else if (result.toString().length > 8) {
+				document.getElementById('display').textContent = result.toFixed(8);
+			} else if (cal.num2 === null) {
+				document.getElementById('display').textContent = cal.num1.toFixed(8);
+			} else {
+				document.getElementById('display').textContent = result;
+			}
+		});
+	};
+
+	this.clearDisplayValue = function () {
+		const clearButton = document.querySelector('#clear-button');
+		clearButton.addEventListener('click', () => {
+			document.getElementById('display').textContent = null;
+			cal.num1 = null;
+			cal.op = null;
+			cal.num2 = null;
+			cal.opCount = 0;
 		});
 	};
 }
@@ -79,3 +121,4 @@ const cal = new Calculator();
 cal.displayNumbers();
 cal.displayOperator();
 cal.equals();
+cal.clearDisplayValue();
